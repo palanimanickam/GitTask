3c88a01ae8020281d58a25e17486fca3eecd95fe
palanimanickam <palanimanickam8055@gmail.com>
2022-11-07 19:59:07 +0530

    MyCountry

--- src/test/java/org/tcs/India.java
+++ src/test/java/org/tcs/India.java
@@ -1,5 +1,13 @@
 package org.tcs;
 
 public class India {
-
+	public void state1() {
+	System.out.println("tamilnadu");
+}
+public void state2() {
+		System.out.println("andra");
+}
+	public void state3() {
+		System.out.println("kerala");			
+}
 }
